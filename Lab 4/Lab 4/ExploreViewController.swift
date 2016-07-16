6076af08a95a05109aa6ce5f04ab932f8f7f44b6
jawjay <mjajeh@wustl.edu>
2016-07-16 11:32:45 -0700

    init

---
+++ Lab 4/Lab 4/ExploreViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lab 4
+//
+//  Created by Mark Jajeh on 7/16/16.
+//  Copyright © 2016 Mark Jajeh. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab 4/Lab 4/FavoritesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lab 4
+//
+//  Created by Mark Jajeh on 7/16/16.
+//  Copyright © 2016 Mark Jajeh. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
